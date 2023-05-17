e2960336d8e7e1ef97cdb7be59cf476d8774a369
AKSUF <abu053125@gmail.com>
2023-05-17 16:51:59 +0600

    datastructure in object

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
-const arr = [1, 2, 3, 4, 'Abu']
-arr.push(5);
-console.log(arr);
-arr.unshift(0); //to add variable at the begining
-console.log(arr);
-arr.pop(); //to remove last element in arry
-console.log(`${arr} pop`);
-arr.shift(); //to remove first element in array
-console.log(`${arr} shift`);
-for (const item of arr) {
-    console.log(`${arr} loop`);
-}
 
+const obj = {
+    name: 'Abu',
+    age: 25,
+    "key-three": function() {
+        console.log(this.name);
+    }
+}
+obj.hobby = 'football',
+    console.log(obj.name);
+console.log(obj['age'])
+console.log(obj['key-three']);
+console.log(obj);
+obj.sayMyName();
 
 
 
